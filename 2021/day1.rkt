cf3f24a07f4dcbca48929dea3b4b12ec93ac7fec
Alex Richey <alexrichey1@gmail.com>
2021-12-05 02:38:07 -0500

    day 1, part 2

--- 2021/day1.rkt
+++ 2021/day1.rkt
@@ -5,14 +5,18 @@
 (require racket/list)
 
 (define into-list
-  (make-effectful-fold-reducer (λ (lst v)
-                                 (cons v lst))
-                               list
-                               reverse))
+  (make-fold-reducer (λ (lst v) (cons v lst))
+                     (list)))
 
 (length (transduce (file->lines  "resources/01.txt")
-           (mapping string->number)
-           (filtering (λ (x) (not (false? x))))
-           (windowing 2)
-           (filtering (λ (p) (> (second p) (first p))))
-           #:into into-list))
+            (mapping string->number)
+            (filtering (λ (x) (not (false? x))))
+            (windowing 3)
+            (windowing 2)
+            (mapping (λ (x)
+                       (list
+                        (apply + (first x))
+                        (apply + (second x)))))
+            (filtering (λ (x) (> (second x) (first x))))
+            #:into into-list))
+
