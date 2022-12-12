4e9c9621d3c1c9ad6a52bc6ab4c4c9808533ce06
Alex Richey <alexrichey1@gmail.com>
2022-12-12 15:26:04 -0500

    Add 2022 Day 4 part 2 in Haskell

--- 2022/haskell/day04.hs
+++ 2022/haskell/day04.hs
@@ -11,10 +11,9 @@ main = do
 
         let ranges = filter (/= "") (splitOn "\n" raw)
             groups = map parseAssignmentGroup ranges
-            withOverlap = filter (\r -> eitherContains (head r) (last r)) groups
+            withOverlap = filter (\r -> hasOverlap (head r) (last r)) groups
 
         print $ length withOverlap
-
         hClose handle
 
 
@@ -26,6 +25,12 @@ parseAssignmentGroup s = [head parts, last parts]
 
 containsRange :: [Int] -> [Int] -> Bool
 containsRange [supL,supR] [subL,subR] = (supL <= subL) && (supR >= subR)
+
+isInRange :: [Int] -> Int -> Bool
+isInRange [r1,r2] i = (i >= r1) && (i <= r2)
+
+hasOverlap :: [Int] -> [Int] -> Bool
+hasOverlap r1@[r1Left,r1Right] r2@[r2Left, r2Right] = (eitherContains r1 r2) || (isInRange r2 r1Left) || (isInRange r2 r1Right)
 
 eitherContains :: [Int] -> [Int] -> Bool
 eitherContains r1 r2 = containsRange r1 r2 || containsRange r2 r1
