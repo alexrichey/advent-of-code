39c9860b05aa5c199dde0584a902520a295ba6fb
Alex Richey <alexrichey1@gmail.com>
2022-12-12 04:22:20 -0500

    Add 2022 Day 3 part 2 in Haskell

--- 2022/haskell/day03.hs
+++ 2022/haskell/day03.hs
@@ -10,11 +10,22 @@ main = do
 
         let letterVals = Map.fromList $ (zip ['a'..'z'] [1..]) ++ (zip ['A'..'Z'] [27..])
 
+        -- Part 1
+        -- let contents = words raw
+        --     partitionedContents = map (\x -> splitAt ((length x) `div` 2) x) contents
+        --     dupedLetters = map (\x -> head $ intersect (fst x) (snd x)) partitionedContents
+        --     vals = map (\c -> Map.lookup c letterVals) dupedLetters
+
+        -- Part 2
         let contents = words raw
-            partitionedContents = map (\x -> splitAt ((length x) `div` 2) x) contents
-            dupedLetters = map (\x -> head $ intersect (fst x) (snd x)) partitionedContents
+            partitionedContents = chunksOf 3 contents
+            dupedLetters = map commonChar partitionedContents
             vals = map (\c -> Map.lookup c letterVals) dupedLetters
 
         print $ fmap sum $ sequence vals
 
         hClose handle
+
+
+commonChar :: [String] -> Char
+commonChar [a,b,c] = head $ intersect a (intersect b c)
