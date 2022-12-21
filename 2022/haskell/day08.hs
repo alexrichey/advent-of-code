044edc24b61e56c319de89d0f3a588660542ced4
Alex Richey <alexrichey1@gmail.com>
2022-12-21 14:12:32 -0500

    Add 2022 Day 08 part 2 in Haskell

--- 2022/haskell/day08.hs
+++ 2022/haskell/day08.hs
@@ -1,32 +1,42 @@
 import System.IO
 import Data.List
-import qualified Data.Map as Map
-import Data.String
 import Data.List.Split
-import qualified Data.Tree as Tree
-import Data.Typeable
+import Data.String
 
 main = do
    handle <- openFile "../inputs/day_08/input.txt" ReadMode
    raw <- hGetContents handle
    let treeHeights = words raw
-    -- "30373"    (0,1) -> (1,0)
+    -- "30373"
     -- "25512"
     -- "65332"
     -- "33549"
     -- "35390"
+   print $ maximum [scenicScore treeHeights (x,y) | x <- [0..(length (head treeHeights)) -1],
+                                                    y <- [0..(length treeHeights)        -1]]
 
-       treeHeightsTranspose = transpose treeHeights
-    -- "32633"
-    -- "05535"
-    -- "35353"
-    -- "71349"
-    -- "32290"
+-- p2
+endsAreVisible :: [Char] -> Bool
+endsAreVisible [] = True
+endsAreVisible (x:[]) = True
+endsAreVisible (x:y:[]) = True
+endsAreVisible (x:xs) = x > maximum (init xs)
 
-   let visible = [isVisibleInGrid treeHeights (x,y) | x <- [0..(length (head treeHeights)) -1],
-                                                      y <- [0..(length treeHeights) -1]]
-   print $ length $ filter (== True) visible
+scenicScoreLtoR :: [Char] -> Int
+scenicScoreLtoR slice = length $ takeWhile endsAreVisible $ drop 2 $ inits slice
 
+scenicScoreInSlice :: [Char] -> Int -> [Int]
+scenicScoreInSlice slice pos = [scenicScoreLtoR $ [el] ++ (reverse lft),
+                                scenicScoreLtoR $ [el] ++ rt]
+  where lft = take pos slice
+        (el:rt) = drop pos slice
+
+scenicScore :: [[Char]] -> (Int, Int) -> Int
+scenicScore grid coords@(x,y) = foldl (*) 1 $ leftRight ++ upDown
+  where leftRight = scenicScoreInSlice (grid !! y) x
+        upDown = scenicScoreInSlice (transpose grid !! x) y
+
+-- p1
 isVisibleInSlice :: [Char] -> Int -> (Bool, Bool)
 isVisibleInSlice slice pos = (null lft || el  > (maximum lft),
                               null rt  || el  > (maximum rt ))
@@ -34,7 +44,7 @@ isVisibleInSlice slice pos = (null lft || el  > (maximum lft),
         (el:rt) = drop pos slice
 
 isVisibleInGrid :: [[Char]] -> (Int, Int) -> Bool
-isVisibleInGrid tree coords@(x,y) = visL || visR || visU || visD
-  where transposeTree = transpose tree
-        (visL, visR) = isVisibleInSlice (tree !! y) x
-        (visU, visD) = isVisibleInSlice (transposeTree !! x) y
+isVisibleInGrid grid coords@(x,y) = visL || visR || visU || visD
+  where transposeGrid = transpose grid
+        (visL, visR) = isVisibleInSlice (grid !! y) x
+        (visU, visD) = isVisibleInSlice (transposeGrid !! x) y
